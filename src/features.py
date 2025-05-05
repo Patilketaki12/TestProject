9feced7cfe7650da17bd7dd1fda92dfbd2c0d74e
Amol Patil <patilketaki12@gmail.com>
2025-05-05 22:14:12 +0200

    first commit

---
+++ src/features.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+
+def simpeimput(df: pd.DataFrame, col: list) -> pd.DataFrame:
+    """
+    Impute missing values in specified columns of a DataFrame using the mean.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame.
+    - columns (list): List of column names to impute.
+
+    Returns:
+    - pd.DataFrame: DataFrame with imputed values.
+    """
+    imputer = SimpleImputer(strategy="mean")
+    df[col] = imputer.fit_transform(df[col])
+    return df
